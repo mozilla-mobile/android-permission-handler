b6873e23f9e1171ba4959f3076d2ae698f7a18b3
Kami <yichen@mozilla.com>
2019-06-12 10:06:00 +0800

    Migrate to AndroidX (#13)

--- app/src/main/java/org/mozilla/permissionhandler/sample/MainActivity.java
+++ app/src/main/java/org/mozilla/permissionhandler/sample/MainActivity.java
@@ -2,14 +2,12 @@ package org.mozilla.permissionhandler.sample;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
